7c6b265db3079e03661cf073fccf41e72e69801e
Chang Xiong <cnxiong03@gmail.com>
2019-08-28 20:58:32 -0400

    change our function into an object using methods

--- todo.js
+++ todo.js
@@ -1,26 +1,18 @@
 var todoList = {
-    todos: [],
+    todos: ["item 1", "item 2", "item 3"],
     displayTodos: function () {
         console.log('My todos:', this.todos);
     },
-    addTodo: function (todoText) {
-        this.todos.push({
-            todoText: todoText,
-            completed: false,
-        });
+    addTodo: function (todo) {
+        this.todos.push(todo);
         this.displayTodos();
     },
-    changeTodo: function (position, todoText) {
-        this.todos[position].todoText = todoText;
+    changeTodo: function (position, newValue) {
+        this.todos[position] = newValue;
         this.displayTodos();
     },
     deleteTodo: function (position) {
         this.todos.splice(position, 1);
-        this.displayTodos();
-    },
-    toggleCompleted: function(position) {
-        var todo = this.todo[position];
-        todo.completed = !todo.completed;
         this.displayTodos();
     }
 };
