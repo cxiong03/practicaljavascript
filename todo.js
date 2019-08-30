bd1711944d2f6834ec2435cd756856f8d413441a
Chang Xiong <cnxiong03@gmail.com>
2019-08-29 20:48:14 -0400

    created object called handler. included methods to modify and clear input fields

--- todo.js
+++ todo.js
@@ -56,21 +56,38 @@ var todoList = {
             for (var i = 0; i < totalTodos; i++) {
                 this.todos[i].completed = true;
             }
-
         }
         this.displayTodos();
     }
 };
 
-// 1. We want to get access to the display todos button.
-var displayTodosButton = document.getElementById("displayTodosButton");
-var toggleAllButton = document.getElementById("toggleAllButton");
-
-// 2. We want to run displayTodos method, when someone clicks the display todos button.
-displayTodosButton.addEventListener("click", function() {
-    todoList.displayTodos();
-});
-
-toggleAllButton.addEventListener("click", function() {
-    todoList.toggleAll();
-});
+var handlers = {
+    displayTodos: function() {
+        todoList.displayTodos();
+    },
+    addTodo: function() {
+        var addTodoTextInput = document.getElementById("addTodoTextInput");
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value= "";
+    },
+    changeTodo: function() {
+        var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
+        var changeTodoTextInput = document.getElementById("changeTodoTextInput");
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = "";
+        changeTodoTextInput.value = "";
+    },
+    deleteTodo: function() {
+        var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = "";
+    },
+    toggleCompleted: function() {
+        var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        this.toggleCompletedPositionInput.value = "";
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
+    }
+};
